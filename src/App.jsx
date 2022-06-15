4c00c61e2960523b0d16052b47f668601d98f2a1
japsolo <jh.docente@gmail.com>
2022-06-14 20:54:35 -0500

    sumé env variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [list, setList] = useState([])
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}?limit=10`)
+      .then(response => response.json())
+      .then(data => {
+        setList(data.products);
+      })
+      .catch(e => console.log(e))
+  }, [])
+
+  console.log(import.meta.env.VITE_API_URL)
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Esta es la página de Javi</p>
+        <p>{import.meta.env.VITE_API_URL}</p>
+        <pre style={{
+          fontSize: "0.9rem"
+        }}>
+          {JSON.stringify(list, null, 2)}
+        </pre>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
